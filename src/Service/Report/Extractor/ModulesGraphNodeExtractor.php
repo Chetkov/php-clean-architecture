2708da55b4d7e732c43d8c9cdff98b751308c357
Валерий Четков <chetkov.valeriy@gmail.com>
2020-05-19 12:07:18 +0300

    расширение информации о компонентах на графе

--- src/Service/Report/Extractor/ModulesGraphNodeExtractor.php
+++ src/Service/Report/Extractor/ModulesGraphNodeExtractor.php
@@ -16,9 +16,23 @@ class ModulesGraphNodeExtractor
      */
     public function extract(Module $node): array
     {
+        $fanIn = array_map(function (Module $inputDependency) {
+            return $inputDependency->name();
+        }, $node->getDependentModules());
+
+        $fanOut = array_map(function (Module $outputDependency) {
+            return $outputDependency->name();
+        }, $node->getDependencyModules());
+
+        $title = 'Abstractness: ' . $node->calculateAbstractnessRate() . '<br>';
+        $title .= 'Instability: ' . $node->calculateInstabilityRate() . '<br>';
+        $title .= 'Fan-in: ' . implode(', ', $fanIn) . '<br>';
+        $title .= 'Fan-out: ' . implode(', ', $fanOut) . '<br>';
+
         return [
             'id' => spl_object_hash($node),
             'label' => $node->name(),
+            'title' => $title,
         ];
     }
 }
