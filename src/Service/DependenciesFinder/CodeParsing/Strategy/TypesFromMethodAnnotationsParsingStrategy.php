be653f5a5a46f798e2f31502e12456abb91bf7da
v.chetkov <chekov.valeriy@gmail.com>
2020-08-01 15:57:21 +0300

    Добавлена стратегия поиска зависимостей в аннотациях @method|@method static

---
+++ src/Service/DependenciesFinder/CodeParsing/Strategy/TypesFromMethodAnnotationsParsingStrategy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Chetkov\PHPCleanArchitecture\Service\DependenciesFinder\CodeParsing\Strategy;
+
+use Chetkov\PHPCleanArchitecture\Helper\StringHelper;
+
+/**
+ * Class TypesFromMethodAnnotationsParsingStrategy
+ * @package Chetkov\PHPCleanArchitecture\Service\DependenciesFinder\CodeParsing\Strategy
+ */
+class TypesFromMethodAnnotationsParsingStrategy implements CodeParsingStrategyInterface
+{
+    /**
+     * Возвращает типы найденные в аннотациях method
+     * @inheritDoc
+     */
+    public function parse(string $content): array
+    {
+        $pattern = '/(?P<annotation>@method[\s]+static|@method)[\s]*(?P<leftTypes>[\w\[\]|\\\\]*)[\s]+(?P<methodName>[^\(\s]+)[\s\(]+(?P<arguments>[^\(\)]*)[\)][\s:]+(?P<rightTypes>[\w\[\]|\\\\]*)/ium';
+        preg_match_all($pattern, $content, $matches);
+
+        $types = array_filter(array_merge($matches['leftTypes'], $matches['rightTypes']));
+
+        foreach ($matches['arguments'] as $argumentsAsString) {
+            $arguments = explode(',', $argumentsAsString);
+            foreach ($arguments as $argumentAsString) {
+                [$variableWithType] = explode('=', $argumentAsString);
+                $argumentParts = array_values(array_filter(explode(' ', $variableWithType)));
+                if (isset($argumentParts[1])) {
+                    $types[] = $argumentParts[0];
+                }
+            }
+        }
+
+        $dependencies = [];
+        foreach ($types as $typesAsString) {
+            foreach (explode('|', str_replace('[]', '', StringHelper::removeSpaces($typesAsString))) as $type) {
+                $dependencies[$type] = true;
+            }
+        }
+
+        return array_keys($dependencies);
+    }
+}
