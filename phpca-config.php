e5daa26e0d7848551f314cbfccdad31b7dc6409a
Valeriy Chetkov <chetkov.valeriy@gmail.com>
2026-05-10 02:19:23 +0300

    config fixes

--- phpca-config.php
+++ phpca-config.php
@@ -17,83 +17,91 @@ use Chetkov\PHPCleanArchitecture\Service\Report\SpaReport\ReportRenderingService
 use Chetkov\PHPCleanArchitecture\Service\Report\ReportRenderingServiceInterface;
 
 return [
-    // Директория в которую будут складываться файлы отчета
+    // Директория, в которую будут складываться файлы отчета.
+    // Значение можно переопределить переменной окружения PHPCA_REPORTS_DIR.
     'reports_dir' => (string) getenv('PHPCA_REPORTS_DIR') ?: __DIR__ . '/phpca-reports',
 
-    // Учет vendor пакетов (каждый подключенный пакет, за исключением перечисленных в excluded, будет представлен компонентом)
+    // Автоматически описывает Composer-пакеты из vendor как внешние компоненты.
+    // Такие компоненты не анализируются изнутри, но зависимости вашего кода от них попадают в отчет и проверки.
     'vendor_based_components' => [
         'enabled' => true,
         'vendor_path' => __DIR__ . '/vendor',
         'excluded' => [
-//            '/excluded/vendor/package/dir',
+            // __DIR__ . '/vendor/vendor/package-to-ignore',
         ],
     ],
 
-    // Общие для всех компонентов ограничения
+    // Общие для всех компонентов ограничения.
+    // Если ключ отсутствует, check_acyclic_dependencies_principle и check_stable_dependencies_principle считаются true.
     'restrictions' => [
-        // Включение/отключение обнаружения нарушений принципа ацикличности зависимостей.
-         'check_acyclic_dependencies_principle' => true,
+        // Проверять нарушения ADP: Acyclic Dependencies Principle.
+        'check_acyclic_dependencies_principle' => true,
 
-        // Включение/отключение обнаружения нарушений принципа устойчивых зависимостей.
-         'check_stable_dependencies_principle' => true,
+        // Проверять нарушения SDP: Stable Dependencies Principle.
+        'check_stable_dependencies_principle' => true,
 
         // Максимально допустимое расстояние до главной диагонали.
-        // Элемент может отсутствовать или быть null, в таком случае ограничения не будут применены.
-//         'max_allowable_distance' => 0.1,
+        // Если ключ отсутствует или равен null, это ограничение не применяется.
+        // 'max_allowable_distance' => 0.1,
     ],
 
-    // Описание компонентов и их ограничений
+    // Описание компонентов и их ограничений.
     'components' => [
         'model' => [
-            // Требуется-ли анализировать содержимое компонента, или он описан исключительно для возможности
-            // сопоставления зависимости других компонентов от элементов текущего?
-            // Значение по умолчанию true (в случае отсутствия его в конфиге).
+            // Анализировать содержимое компонента или только использовать его как внешний компонент,
+            // с которым можно сопоставить зависимости других компонентов.
+            // Если ключ отсутствует, значение считается true.
             'is_analyze_enabled' => true,
             'name' => 'model',
             'roots' => [
                 [
+                    // Корневой путь компонента. Можно указать директорию или отдельный файл.
+                    // Анализатор читает PHP-файлы через AST, поэтому PSR-4 совпадение имени класса и пути не обязательно.
                     'path' => __DIR__ . '/src/Model',
+
+                    // Namespace используется для fallback-имен executable scripts и файлов без объявленных символов,
+                    // а также помогает сопоставлять зависимости, когда у найденного элемента нет известного пути.
                     'namespace' => 'Chetkov\PHPCleanArchitecture\Model',
                 ],
-                // Иногда, особенно в старых проектах, код логически относящийся к одному компоненту, разбросан по разным частям
-                // системы. В таком случае можно указать в конфиге несколько корневых директорий и, т.о. отнести их содержимое
-                // какому-то одному компоненту.
+                // Если код одного компонента лежит в нескольких местах, добавьте несколько root-ов.
                 //
                 // [
                 //     'path' => '/path/to/component/first',
                 //     'namespace' => 'Component\First',
                 // ],
             ],
-            //Директории или файлы, которые будут пропущены в процессе анализа
+            // Директории или файлы, которые будут пропущены при анализе этого компонента.
             'excluded' => [
-//                '/path/to/First/Component/dir1',
-//                '/path/to/First/Component/dir2',
+                // '/path/to/First/Component/dir1',
+                // '/path/to/First/Component/dir2',
             ],
             'restrictions' => [
-                // Имеет приоритет над общей настройкой restrictions->max_allowable_distance
+                // Имеет приоритет над общей настройкой restrictions.max_allowable_distance.
                 // 'max_allowable_distance' => 0.1,
 
                 // Список РАЗРЕШЕННЫХ исходящих зависимостей. Заполняется именами других компонентов.
-                // Может отсутствовать, быть [] или null, в таком случае никакие ограничения накладываться не будут.
-                // Не должен использоваться совместно с forbidden_dependencies!
-                 'allowed_dependencies' => ['model'],
+                // Если ключ отсутствует, равен [] или null, список разрешенных зависимостей не ограничивается.
+                // Не используйте одновременно с forbidden_dependencies.
+                'allowed_dependencies' => ['model'],
 
                 // Список ЗАПРЕЩЕННЫХ исходящих зависимостей. Заполняется именами других компонентов.
-                // Может отсутствовать, быть [] или null, в таком случае никакие ограничения накладываться не будут.
-                // Не должен использоваться совместно с allowed_dependencies!
+                // Если ключ отсутствует, равен [] или null, список запрещенных зависимостей не ограничивается.
+                // Не используйте одновременно с allowed_dependencies.
                 // 'forbidden_dependencies' => ['ThirdComponent'],
 
-                // Список публичных элементов компонента. Если отсутствует или пустой, все элементы считаются публичными.
+                // Список публичных элементов компонента: FQCN, директории или файлы.
+                // Если отсутствует или пустой, все элементы считаются публичными.
                 // Если не пустой, не перечисленные в списке элементы будут считаться приватными.
-                // Не должен использоваться совместно с private_elements!
+                // Не используйте одновременно с private_elements.
                 // 'public_elements' => [
                 //     First\Component\FirstClass::class,
                 //     First\Component\SecondClass::class,
                 //     __DIR__ . '/directory/with/public/elements',
                 // ],
 
-                // Список приватных элементов компонента. Если отсутствует или пустой, все элементы считаются публичными.
-                // Не должен использоваться совместно с public_elements!
+                // Список приватных элементов компонента: FQCN, директории или файлы.
+                // Если отсутствует или пустой, все элементы считаются публичными.
+                // Не используйте одновременно с public_elements.
                 // 'private_elements' => [
                 //     First\Component\FirstClass::class,
                 //     First\Component\SecondClass::class,
@@ -148,31 +156,36 @@ return [
         ],
     ],
 
-    // Исключения
+    // Исключения.
     'exclusions' => [
+        // Разрешенное текущее состояние для постепенного внедрения инструмента в существующий проект.
+        // Файл генерируется командой:
+        // vendor/bin/phpca-allow-current-state phpca-config.php
+        //
+        // Если enabled=true, но файл storage еще не существует, allowed state просто не применяется.
         'allowed_state' => [
             'enabled' => false,
             'storage' => __DIR__ . '/phpca-allowed-state.php',
         ],
     ],
 
+    // Технические фабрики. В обычном проекте этот блок, как правило, не нужно менять.
     'factories' => [
-        //Фабрика, собирающая DependenciesFinder
+        // Фабрика, собирающая DependenciesFinder.
         'dependencies_finder' => static function (): DependenciesFinderInterface {
             return new AstDependenciesFinder();
         },
-        //Фабрика, собирающая сервис рендеринга отчетов
+        // Фабрика, собирающая сервис рендеринга отчетов.
         'report_rendering_service' => static function (EventManagerInterface $eventManager): ReportRenderingServiceInterface {
             return new ReportRenderingService($eventManager);
         },
-        //Фабрика, собирающая и настраивающая EventManager
+        // Фабрика, собирающая и настраивающая EventManager.
         'event_manager' => static function (): EventManagerInterface {
             return new EventManager([
                 new ReportBuildingEventListener(),
                 new AnalysisEventListener(),
                 new ComponentAnalysisEventListener(),
                 new FileAnalyzedEventListener(),
-                new ReportBuildingEventListener(),
                 new ReportRenderingEventListener(),
                 new ComponentReportRenderingEventListener(),
                 new UnitOfCodeReportRenderedEventListener(),
